f8b86a7147a115ed1a7b8ee496f7d692700ec65d
Ganesh Sharma <gsk1664@gmail.com>
2020-10-13 09:22:37 +0530

    main section data file

---
+++ shoping-app/src/components/pages/HomePage/Data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    lightBg: false ,
+    lightText: false , 
+    lightTextDesc: true ,
+    topLine: "Exclusive Access" , 
+    headline: "Unlimited tranction with Zero fees" , 
+    description: "Get access to our exclusive diamond card that unlimites tranction without getting charged my fees" , 
+    buttonLabel: "Get started" , 
+    imgStart: '' ,
+    img: '' ,
+    alt: "Credit Card"
+}
